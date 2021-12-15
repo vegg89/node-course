2fc2ecab6258b6542bf2bc0170200ec65df54ac5
Vincent Gutierrez <vincentgg89@gmail.com>
2021-12-15 02:53:49 -0600

    Initial commit

---
+++ notes-app/app.js
@@ -0,0 +1,57 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Creater add command
+yargs.command({
+    command: 'add',
+    describe: 'Add new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function (){
+        console.log('Removing a note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function (){
+        console.log('Listing notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function (){
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse()
